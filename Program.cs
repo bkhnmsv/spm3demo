0db05edb5c29951ff1393d2c5ac48a662208478c
Bekhan Musaev <bek154@outlook.at>
2023-11-30 14:15:43 +0100

    Update console output with m1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! Git is cool! - Really? - YESSS - change");
+Console.WriteLine("Hello, World! Git is cool! - Really? - YESSS - change m1");
