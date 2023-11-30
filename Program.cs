cdcd1b098907a96ebd6132ab1769773081afb76d
Bekhan Musaev <bek154@outlook.at>
2023-11-30 16:28:43 +0100

    add f3 and f4

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+<<<<<<< HEAD
 Console.WriteLine("Hello, World! Git is cool! - Really? - YESSS - change m1 f1 remote change f2");
+=======
+Console.WriteLine("Hello, World! Git is cool! - Really? - YESSS - change m1 f1 f2 f3 f4");
+>>>>>>> f9f4deb (add f3 and f4)
